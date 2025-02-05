104ca52d0af68bd96a21dcd42fabcca3675f0e9f
Alexander Makevnin <a.makevnin@gmail.com>
2025-02-05 19:20:54 +0300

    build.grdle.kts update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     application
     id("java")
     checkstyle
+    jacoco
 }
 
 group = "hexlet.code"
@@ -12,10 +13,24 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.3")
+
 }
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+    testLogging {
+        showStandardStreams = true
+    }
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        xml.required = true
+        html.required = true
+    }
+
 }
